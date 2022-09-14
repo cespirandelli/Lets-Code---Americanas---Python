db2cd9f276c57120b83bc5875e943d745a85dc58
cespirandelli <48394686+cespirandelli@users.noreply.github.com>
2022-09-14 18:52:52 -0300

    Primeiro programa Coding Tank
    Comandos PRINT e INPUT

---
+++ primeiro.py
@@ -0,0 +1,14 @@
+# Algoritmo, conjunto de instruções ou etapas 
+# para resolver um problema.
+
+# 1a Etapa -  Entrada de dados ("ingredientes")
+# 2a Etapa -  Processamento ("receita")
+# 3a Etapa -  Saída de dados ("bolo")
+
+
+print('Hello World!')
+print('Bem vindo ao curso de Python \
+do Coding Tank!')
+
+# Comando input() - Entrada de dados
+input('Qual é a linguagem de programação que você está estudando? ')
