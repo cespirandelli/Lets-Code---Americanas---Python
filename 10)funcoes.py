78b1e4dd6612cf9bd1bb15fb79a1221daa65e4e2
cespirandelli <48394686+cespirandelli@users.noreply.github.com>
2022-09-15 19:54:14 -0300

    Update 10)funcoes.py
    Função com e sem parâmetro

--- 10)funcoes.py
+++ 10)funcoes.py
@@ -15,9 +15,29 @@ type()         # Retorna o tipo de algo
 max()          # Retorna o maior valor de algo
 '''
 
-# Função inicial
-def funcao(a):
-    print('Olá, sou uma função', a)
+            # # Função inicial
+            # def funcao(a):
+            #     print('Olá, sou uma função', a)
 
-# Chamando a função
-funcao('teste')
+            # # Chamando a função
+            # funcao('teste')
+
+# Função inicial (com parâmetros)
+
+def saudacao(nome):
+    print(f'\nOlá! Seja muito bem vindo(a) {nome} aqui você irá aprender a programar!!') 
+    # utilizar f-string fica mais polido, do que separação de virgula.
+    print('É um prazer tê-lo por aqui, espero que se dedique no curso (:\n')
+
+saudacao('César')       # funções podem ser chamadas para executar um bloco de código de forma simples
+saudacao('Beatriz')     # como esta função recebe o parâmetro "nome", é necessário escrever uma sting quando
+saudacao('Judite')      # a função é chamada, mas podemos criar funções sem parâmetros
+
+# Funcão sem parâmetros
+
+def saudacao2():
+    print('Salve salve, seja muito bem vindo ao curso de Python!')
+    print('Este é um exemplo de função que não recebe parâmetros\n')
+
+saudacao2()
+
