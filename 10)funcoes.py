7c48ef3ffbf540ff7328283d1dda109ecfcc4c12
cespirandelli <48394686+cespirandelli@users.noreply.github.com>
2022-09-15 19:38:31 -0300

    Create 10)funcoes.py

---
+++ 10)funcoes.py
@@ -0,0 +1,23 @@
+# O que são funções? 
+# Algo que é criado para ser utilizado repetidas vezes.
+
+'''
+    Quero saber quantos elementos tenho dentro de uma lista. Posso usar um for loop ou while loop? 
+    Sim, mas é mais fácil usar a função len(), pois ela já existe e é mais fácil de usar.
+'''
+
+'''
+# Funções já utilizadas anteriormente:
+print()        # Imprime algo na tela
+input()        # Pega uma entrada do usuário
+len()          # Retorna o tamanho de algo
+type()         # Retorna o tipo de algo
+max()          # Retorna o maior valor de algo
+'''
+
+# Função inicial
+def funcao(a):
+    print('Olá, sou uma função', a)
+
+# Chamando a função
+funcao('teste')
