9249f8997c006d4850a7dcf4d9495e1e9b0863d1
cespirandelli <48394686+cespirandelli@users.noreply.github.com>
2022-09-15 14:11:15 -0300

    Create 8)listas.py
    Introdução às listas, estruturas de dados

---
+++ 8)listas.py
@@ -0,0 +1,26 @@
+'''
+    Para que servem as listas?                       Para armazenar dados
+    
+    Como criar uma lista?                            Usando colchetes []
+    
+    Como adicionar um item a uma lista?              Usando o método append() ou insert()
+    
+    Como remover um item de uma lista?               Usando o método remove(), pop() ou del()
+    
+    Como ordenar uma lista?                          Usando o método sort()
+    
+    Como inverter uma lista?                         Usando o método reverse()
+    
+    Como contar quantos itens uma lista possui?      Usando a função len()
+    
+    Como descobrir o índice de um item de uma lista? Usando o método index()
+    
+    Como acessar um item de uma lista?               Usando o índice do item entre colchetes []
+    
+    Como acessar um intervalo de itens de uma lista? Usando o índice do primeiro item e do último item entre colchetes []
+    
+    Como acessar os últimos itens de uma lista?      Usando o índice -1 e -2 e assim por diante
+'''
+
+# Criando uma lista
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
